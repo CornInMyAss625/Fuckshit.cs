761b3d77d7f5493d0682778ba1611a464d4d66c1
CornInMyAss625 <156862865+CornInMyAss625@users.noreply.github.com>
2024-06-20 14:45:51 +0100

    Create FuckShitMain.cs

---
+++ FuckShitMain.cs
@@ -0,0 +1,66 @@
+int multiplication(int number, int number_2)
+{
+    return number * number_2
+}
+
+int multiplication = 2;
+
+int num_1 = 1;
+do
+{
+    Console.WriteLine("This is a do loop");
+} while (num_1 < 1);
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("This is a for loop");
+}
+
+
+//BreakInCode1
+int num_2 = 5;
+while (num_2 == 5)
+{
+    Console.WriteLine("This is a while loop");
+}
+
+if (num_1 != 5)
+{
+    Console.WriteLine("This is an if check");
+}
+
+if (num_1 == 5)
+{
+
+}
+else if (num_1 != 5)
+{
+    Console.WriteLine("This is an if else if check");
+}
+
+if (num_1 == 5)
+{
+    
+}
+else
+{
+    Console.WriteLine("This is an if else statement");
+}
+
+int num_3 = 1;
+switch (num_3)
+{
+
+    case 1:
+        Console.WriteLine("This is a Monday (switch statement)");
+        break;
+    case 2:
+        Console.WriteLine("This is a Tuesday (switch statement)");
+        break;
+    case 3:
+        Console.WriteLine("This is a Wednesday (switch statement)");
+        break;
+    default:
+        Console.WriteLine("Error");
+        break;
+}
